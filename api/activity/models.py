cb8def1e350ce2ffb47df1bc1c835bf72fab2f83
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2023-04-20 17:55:56 +0300

    Auto stash before merge of "main" and "origin/main"

--- api/activity/models.py
+++ api/activity/models.py
@@ -1,6 +1,5 @@
 import uuid
 from django.db import models
-
 from dirtyfields import DirtyFieldsMixin
 
 
--- api/confirmation/models.py
+++ api/confirmation/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 CONFIRM_TYPE_CHOICES = (
-    ('Create', 'Create'),
-    ('Update', 'Update'),
-)
+        ('Create', 'Create'),
+        ('Update', 'Update'),
+    )
 
 
 class Confirmation(models.Model):
+
     activity = models.ForeignKey('activity.Activity', on_delete=models.CASCADE, related_name='activity', null=True,
                                  blank=True)
     employee = models.ForeignKey('account.MyUser', on_delete=models.CASCADE, related_name='user', null=True)
@@ -15,18 +16,5 @@ class Confirmation(models.Model):
     confirm_status = models.BooleanField(default=False)
     confirm_type = models.CharField(max_length=8, blank=True, null=True, choices=CONFIRM_TYPE_CHOICES)
 
-    def str(self):
+    def __str__(self):
         return self.activity.title
-
-
-class AdvertisementConfirmation(models.Model):
-    advertisement = models.ForeignKey('advertisement.Advertisement', on_delete=models.CASCADE,
-                                      related_name='advertisement', null=True, blank=True)
-    employee = models.ForeignKey('account.MyUser', on_delete=models.CASCADE, related_name='employee', null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    confirm_status = models.BooleanField(default=False)
-    confirm_type = models.CharField(max_length=8, blank=True, null=True, choices=CONFIRM_TYPE_CHOICES)
-
-    def str(self):
-        return self.advertisement.title
